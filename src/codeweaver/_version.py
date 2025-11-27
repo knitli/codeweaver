54799509270db55899446f1ecb94c875766b313e
Adam Poulemanos <89049923+bashandbone@users.noreply.github.com>
2025-11-27 14:36:28 -0500

    fix:add back _version.py
    Signed-off-by: Adam Poulemanos <89049923+bashandbone@users.noreply.github.com>

---
+++ src/codeweaver/_version.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2025 Knitli Inc.
+# SPDX-FileContributor: Adam Poulemanos <adam@knit.li>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+"""CodeWeaver's current version."""
+from typing import Final
+
+__version__: Final[str] = "v0.1.0a1"
+
+__all__ = ("__version__",)
