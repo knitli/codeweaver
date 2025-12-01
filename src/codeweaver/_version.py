1a271c8e6fe67b71e46191d738b9a1527dc77f26
Adam Poulemanos <adam@knit.li>
2025-12-01 15:33:18 -0500

    remove _version

--- src/codeweaver/_version.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Knitli Inc.
-# SPDX-License-Identifier: MIT OR Apache-2.0
-"""Version information for CodeWeaver."""
-from typing import Final
-
-
-__version__: Final[str] = "0.0.1rc2+ge7544aa"
-
-__all__ = ("__version__",)
