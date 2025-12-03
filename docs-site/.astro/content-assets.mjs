9081284c8bb3a15ffe1d9c53e91de610375b9800
Adam Poulemanos <adam@knit.li>
2025-12-03 17:35:15 -0500

    Update index.mdx with new tagline and command syntax; remove license file for debugging

--- docs-site/.astro/content-assets.mjs
+++ docs-site/.astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_1SCilo from "../../assets/codeweaver-primary.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
+export default new Map([["../../assets/codeweaver-primary.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_1SCilo]]);
+		
