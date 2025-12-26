8186538614aec6cf72350185dfb3acabc89f5990
Adam Poulemanos <adam@knit.li>
2025-12-26 01:02:21 -0500

    fix: reuse compliance

--- src/codeweaver/core/utils/__init__.py
+++ src/codeweaver/core/utils/__init__.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Knitli Inc.
+# SPDX-FileContributor: Adam Poulemanos <adam@knit.li>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
 """Core utility functions for (most) CodeWeaver packages."""
 
 from __future__ import annotations
--- src/codeweaver/core/utils/generation.py
+++ src/codeweaver/core/utils/generation.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Knitli Inc.
+# SPDX-FileContributor: Adam Poulemanos <adam@knit.li>
+#
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
 """Utility functions for generating identifiers and hashes."""
 
 from __future__ import annotations
