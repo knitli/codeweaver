b130666d4f47c45096265471b0b79938ac23da8b
Adam Poulemanos <adam@knit.li>
2026-03-16 07:32:24 -0400

    fix: namespace conflict preventing initialization when Fastembed provider enabled

--- test2.py
+++
@@ -1,8 +0,0 @@
-from typing import get_origin, Union
-import types
-
-def is_union(a):
-    origin = get_origin(a)
-    return origin is Union or origin is types.UnionType
-
-print(is_union(Union[None]))
