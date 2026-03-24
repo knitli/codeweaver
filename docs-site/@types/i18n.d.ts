66c2ee6108a3a5948ce7b21fca6555be8154106b
Adam Poulemanos <adam@knit.li>
2026-03-24 13:16:08 -0400

    chore: remove old/redundant files update to typescript 6

---
+++ docs-site/@types/i18n.d.ts
@@ -0,0 +1,8 @@
+//SPDX-FileCopyrightText: 2026 Knitli Inc.
+// SPDX-FileContributor: Adam Poulemanos <adam@knit.li>
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+interface Locale extends Intl.Locale {
+    language: string;
+}
